5aba7cd08c75e09af193a357b7c16fca4fc58e24
Derek_TH_Wang <416338223@qq.com>
2020-07-07 16:20:45 +0800

    add robot pose msg
    Change-Id: I157173a78b60404cbb6264c748944119abe15118

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,38 @@
 #include <sensor_msgs/Joy.h>
 
 ros::Subscriber gamepadDataSub;
-ros::Publisher cmdVelPub;
+ros::Publisher cmdVelPub, cmdPosePub;
 
 void SubGamepadData(const sensor_msgs::Joy& msg) {
-  geometry_msgs::Twist cmdVel;
+  geometry_msgs::Twist cmdVel, cmdPose;
   cmdVel.linear.x = msg.axes[1];
   cmdVel.linear.y = msg.axes[0];
   cmdVel.angular.x = -msg.axes[3];
   cmdVel.angular.y = msg.axes[4];
   cmdVelPub.publish(cmdVel);
+
+  if (msg.buttons[3] > 0.1) {  // Y
+    cmdPose.angular.x = 1;
+  } else if (msg.buttons[0] > 0.1) {  // A
+    cmdPose.angular.x = -1;
+  } else {
+    cmdPose.angular.x = 0;
+  }
+  if (msg.buttons[1] > 0.1) {  // Y
+    cmdPose.angular.y = 1;
+  } else if (msg.buttons[2] > 0.1) {  // A
+    cmdPose.angular.y = -1;
+  } else {
+    cmdPose.angular.y = 0;
+  }
+  if (msg.buttons[4] > 0.1) { // Left
+    cmdPose.angular.z = 1;
+  } else if (msg.buttons[5] > 0.1) { // Right
+    cmdPose.angular.z = -1;
+  } else {
+    cmdPose.angular.z = 0;
+  }
+  cmdPosePub.publish(cmdPose);
 }
 
 int main(int argc, char** argv) {
@@ -24,6 +47,7 @@ int main(int argc, char** argv) {
 
   gamepadDataSub = n.subscribe("/joy", 10, &SubGamepadData);
   cmdVelPub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  cmdPosePub = n.advertise<geometry_msgs::Twist>("/cmd_pose", 10);
 
   ros::spin();
   return 0;
